36e2a31c40a200ae1790893a4ec03fa2988173c3
waeeeell <waelelk053@gmail.com>
2025-10-21 19:19:25 +0200

    Post Error Finalitzt

--- AndroidStudioProjects/Activitat04/app/src/main/java/com/example/activitat04/ResultadoIMCPantalla2.kt
+++ AndroidStudioProjects/Activitat04/app/src/main/java/com/example/activitat04/ResultadoIMCPantalla2.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class ResultadoIMCPantalla2 : AppCompatActivity() {
 
     private lateinit var informativo: TextView
